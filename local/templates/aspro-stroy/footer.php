7ae16677b17fecbc6e7354e3648c43b6b07ffcb0
p.reutov <reutov@caweb.ru>
2021-08-12 12:14:31 +0800

    add crm

--- local/templates/aspro-stroy/footer.php
+++ local/templates/aspro-stroy/footer.php
@@ -209,11 +209,6 @@
             );?>
         </div>
         <?CStroy::SetMeta();?>
-                    <script>
-                        (function(w,d,u){
-                            var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
-                            var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-                        })(window,document,'https://crm.strlog.ru/upload/crm/site_button/loader_6_qyoaeq.js');
-                    </script>
+
     </body>
 </html>
