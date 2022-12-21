2664ba5462281dc632ae66fecf1f0f698a361795
p.reutov <reutov@caweb.ru>
2022-12-21 10:21:49 +0800

    текст доставки

--- include/detail/delivery.php
+++ include/detail/delivery.php
@@ -5,6 +5,6 @@
         <p><i class="fa fa-angle-right red-color" aria-hidden="true"></i>Доставим готовые изделия в любой район г. Иркутска и области.</p>
         <p><i class="fa fa-angle-right red-color" aria-hidden="true"></i>Стоимость доставки и дополнительных услуг рассчитывается индивидуально в зависимости от габаритов конструкции и удаленности объекта.</p>
         <p><i class="fa fa-angle-right red-color" aria-hidden="true"></i>Доставка в другой город производится через транспортные компании.</p>
-        <span><a href="/info/articles/usloviya-dostavki/" class="red-color">Читать полностью</a></span>
+        <span><a href="/info/articles/usloviya-dostavki/" class="red-color">Карта бесплатной доставки</a></span>
     </div>
 </div>
