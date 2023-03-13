490cdd155136ced79b759beeddff528acc8ff7b2
p.reutov <reutov@caweb.ru>
2023-03-13 14:20:31 +0800

    меняю местами список разделов и элементов в воротах

--- local/templates/aspro-stroy/components/bitrix/news/newcatalog/section.php
+++ local/templates/aspro-stroy/components/bitrix/news/newcatalog/section.php
@@ -36,133 +36,188 @@ $arSubSections = CCache::CIblockSection_GetList(array("CACHE" => array("TAG" => 
 		<div class="alert alert-warning"><?=GetMessage("SECTION_EMPTY")?></div>
 	<?endif;?>
     <div class="catalog item-views table">
-    <div class="row items" itemscope itemtype="http://schema.org/ItemList">
-	<?if($arSubSections):?>
-		<?// sections list?>
-		<?$APPLICATION->IncludeComponent(
-			"bitrix:news.list",
-            $sectionTemplate,
-			Array(
-				"IBLOCK_TYPE"	=>	$arParams["IBLOCK_TYPE"],
-				"IBLOCK_ID"	=>	$arParams["IBLOCK_ID"],
-				"NEWS_COUNT"	=>	$arParams["NEWS_COUNT"],
-				"SORT_BY1"	=>	$arParams["SORT_BY1"],
-				"SORT_ORDER1"	=>	$arParams["SORT_ORDER1"],
-				"SORT_BY2"	=>	$arParams["SORT_BY2"],
-				"SORT_ORDER2"	=>	$arParams["SORT_ORDER2"],
-				"FIELD_CODE"	=>	$arParams["LIST_FIELD_CODE"],
-				"PROPERTY_CODE"	=>	$arParams["LIST_PROPERTY_CODE"],
-				"DETAIL_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["detail"],
-				"SECTION_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
-				"IBLOCK_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["news"],
-				"DISPLAY_PANEL"	=>	$arParams["DISPLAY_PANEL"],
-				"SET_TITLE"	=>	"N",
-				"SET_STATUS_404" => "N",
-				"INCLUDE_IBLOCK_INTO_CHAIN"	=>	"N",
-				"ADD_SECTIONS_CHAIN"	=>	"N",
-				"CACHE_TYPE"	=>	$arParams["CACHE_TYPE"],
-				"CACHE_TIME"	=>	$arParams["CACHE_TIME"],
-				"CACHE_FILTER"	=>	$arParams["CACHE_FILTER"],
-				"CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
-				"DISPLAY_TOP_PAGER"	=>	$arParams["DISPLAY_TOP_PAGER"],
-				"DISPLAY_BOTTOM_PAGER"	=>	$arParams["DISPLAY_BOTTOM_PAGER"],
-				"PAGER_TITLE"	=>	$arParams["PAGER_TITLE"],
-				"PAGER_TEMPLATE"	=>	$arParams["PAGER_TEMPLATE"],
-				"PAGER_SHOW_ALWAYS"	=>	$arParams["PAGER_SHOW_ALWAYS"],
-				"PAGER_DESC_NUMBERING"	=>	$arParams["PAGER_DESC_NUMBERING"],
-				"PAGER_DESC_NUMBERING_CACHE_TIME"	=>	$arParams["PAGER_DESC_NUMBERING_CACHE_TIME"],
-				"PAGER_SHOW_ALL" => $arParams["PAGER_SHOW_ALL"],
-				"DISPLAY_DATE"	=>	$arParams["DISPLAY_DATE"],
-				"DISPLAY_NAME"	=>	$arParams["DISPLAY_NAME"],
-				"DISPLAY_PICTURE"	=>	$arParams["DISPLAY_PICTURE"],
-				"DISPLAY_PREVIEW_TEXT"	=>	$arParams["DISPLAY_PREVIEW_TEXT"],
-				"PREVIEW_TRUNCATE_LEN"	=>	$arParams["PREVIEW_TRUNCATE_LEN"],
-				"ACTIVE_DATE_FORMAT"	=>	$arParams["LIST_ACTIVE_DATE_FORMAT"],
-				"USE_PERMISSIONS"	=>	$arParams["USE_PERMISSIONS"],
-				"GROUP_PERMISSIONS"	=>	$arParams["GROUP_PERMISSIONS"],
-				"FILTER_NAME"	=>	$arParams["FILTER_NAME"],
-				"HIDE_LINK_WHEN_NO_DETAIL"	=>	$arParams["HIDE_LINK_WHEN_NO_DETAIL"],
-				"CHECK_DATES"	=>	$arParams["CHECK_DATES"],
-				"PARENT_SECTION" => $arResult["VARIABLES"]["SECTION_ID"],
-				"PARENT_SECTION_CODE" => $arResult["VARIABLES"]["SECTION_CODE"],
-			),
-			$component
-		);?>
-	<?endif;?>
-	<?// section elements?>
-	<?include_once("include_filter.php");?>
-	<?
-	$frame = new \Bitrix\Main\Page\FrameHelper('catalog-elements-block');
-	$frame->begin();
-	$frame->setAnimation(true);
-	?>
-<?//include_once("include_sort.php");?>
-	<?//$display_template = $display ? 'catalog-'.$display: 'catalog-table';?>
-	<?$display_template = 'catalog-table';?>
-	
-	<?$APPLICATION->IncludeComponent(
-		"bitrix:news.list",
-		$display_template,
-		array(
-			"S_ASK_QUESTION" => $arParams["S_ASK_QUESTION"],
-			"S_ORDER_PRODUCT" => $arParams["S_ORDER_PRODUCT"],
-			"S_DETAIL_PRODUCT" => $arParams["S_DETAIL_PRODUCT"],			
-			"T_GALLERY" => $arParams["T_GALLERY"],
-			"T_DOCS" => $arParams["T_DOCS"],
-			"T_PROJECTS" => $arParams["T_PROJECTS"],
-			"T_CHARACTERISTICS" => $arParams["T_CHARACTERISTICS"],
-			"IBLOCK_TYPE"	=>	$arParams["IBLOCK_TYPE"],
-			"IBLOCK_ID"	=>	$arParams["IBLOCK_ID"],
-			"NEWS_COUNT"	=>	$arParams["NEWS_COUNT"],
-			"SORT_BY1"	=>	$arAvailableSort[$sort]["SORT"],
-			"SORT_ORDER1"	=>	strtoupper($order),
-			"SORT_BY2"	=>	$arParams["SORT_BY2"],
-			"SORT_ORDER2"	=>	$arParams["SORT_ORDER2"],
-			"FIELD_CODE"	=>	$arParams["LIST_FIELD_CODE"],
-			"PROPERTY_CODE"	=>	$arParams["LIST_PROPERTY_CODE"],
-			"SHOW_SLIDE_PROP"	=>	$arParams["SHOW_SLIDE_PROP"],
-			"DISPLAY_PANEL"	=>	$arParams["DISPLAY_PANEL"],
-			"SET_TITLE"	=>	$arParams["SET_TITLE"],
-			"SET_STATUS_404" => $arParams["SET_STATUS_404"],
-			"INCLUDE_IBLOCK_INTO_CHAIN"	=>	$arParams["INCLUDE_IBLOCK_INTO_CHAIN"],
-			"ADD_SECTIONS_CHAIN"	=>	$arParams["ADD_SECTIONS_CHAIN"],
-			"CACHE_TYPE"	=>	$arParams["CACHE_TYPE"],
-			"CACHE_TIME"	=>	$arParams["CACHE_TIME"],
-			"CACHE_FILTER"	=>	$arParams["CACHE_FILTER"],
-			"CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
-			"DISPLAY_TOP_PAGER"	=>	$arParams["DISPLAY_TOP_PAGER"],
-			"DISPLAY_BOTTOM_PAGER"	=>	$arParams["DISPLAY_BOTTOM_PAGER"],
-			"PAGER_TITLE"	=>	$arParams["PAGER_TITLE"],
-			"PAGER_TEMPLATE"	=>	$arParams["PAGER_TEMPLATE"],
-			"PAGER_SHOW_ALWAYS"	=>	$arParams["PAGER_SHOW_ALWAYS"],
-			"PAGER_DESC_NUMBERING"	=>	$arParams["PAGER_DESC_NUMBERING"],
-			"PAGER_DESC_NUMBERING_CACHE_TIME"	=>	$arParams["PAGER_DESC_NUMBERING_CACHE_TIME"],
-			"PAGER_SHOW_ALL" => $arParams["PAGER_SHOW_ALL"],
-			"DISPLAY_DATE"	=>	$arParams["DISPLAY_DATE"],
-			"DISPLAY_NAME"	=>	$arParams["DISPLAY_NAME"],
-			"DISPLAY_PICTURE"	=>	$arParams["DISPLAY_PICTURE"],
-			"DISPLAY_PREVIEW_TEXT"	=>	$arParams["DISPLAY_PREVIEW_TEXT"],
-			"PREVIEW_TRUNCATE_LEN"	=>	$arParams["PREVIEW_TRUNCATE_LEN"],
-			"ACTIVE_DATE_FORMAT"	=>	$arParams["LIST_ACTIVE_DATE_FORMAT"],
-			"USE_PERMISSIONS"	=>	$arParams["USE_PERMISSIONS"],
-			"GROUP_PERMISSIONS"	=>	$arParams["GROUP_PERMISSIONS"],
-			"FILTER_NAME"	=>	$arParams["FILTER_NAME"],
-			"HIDE_LINK_WHEN_NO_DETAIL"	=>	$arParams["HIDE_LINK_WHEN_NO_DETAIL"],
-			"CHECK_DATES"	=>	$arParams["CHECK_DATES"],
-			"PARENT_SECTION"	=>	$arResult["VARIABLES"]["SECTION_ID"],
-			"PARENT_SECTION_CODE"	=>	$arResult["VARIABLES"]["SECTION_CODE"],
-			"DETAIL_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["detail"],
-			"SECTION_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
-			"IBLOCK_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["news"],
-			"INCLUDE_SUBSECTIONS" => "N",
-			"SHOW_DETAIL_LINK" => $arParams["SHOW_DETAIL_LINK"],
-			"COUNT_IN_LINE" => "3",
-			"IMAGE_POSITION" => "left",
-            "SHOW_PRICE_BLOCK" => $showPriceBlock
-		),
-		$component
-	);?>
-    </div>
+        <div class="row items" itemscope itemtype="http://schema.org/ItemList">
+            <?if($arSubSections && ((int)$arResult['VARIABLES']['SECTION_ID'] !== 13)):?>
+                <?// sections list?>
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:news.list",
+                    $sectionTemplate,
+                    Array(
+                        "IBLOCK_TYPE"	=>	$arParams["IBLOCK_TYPE"],
+                        "IBLOCK_ID"	=>	$arParams["IBLOCK_ID"],
+                        "NEWS_COUNT"	=>	$arParams["NEWS_COUNT"],
+                        "SORT_BY1"	=>	$arParams["SORT_BY1"],
+                        "SORT_ORDER1"	=>	$arParams["SORT_ORDER1"],
+                        "SORT_BY2"	=>	$arParams["SORT_BY2"],
+                        "SORT_ORDER2"	=>	$arParams["SORT_ORDER2"],
+                        "FIELD_CODE"	=>	$arParams["LIST_FIELD_CODE"],
+                        "PROPERTY_CODE"	=>	$arParams["LIST_PROPERTY_CODE"],
+                        "DETAIL_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["detail"],
+                        "SECTION_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
+                        "IBLOCK_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["news"],
+                        "DISPLAY_PANEL"	=>	$arParams["DISPLAY_PANEL"],
+                        "SET_TITLE"	=>	"N",
+                        "SET_STATUS_404" => "N",
+                        "INCLUDE_IBLOCK_INTO_CHAIN"	=>	"N",
+                        "ADD_SECTIONS_CHAIN"	=>	"N",
+                        "CACHE_TYPE"	=>	$arParams["CACHE_TYPE"],
+                        "CACHE_TIME"	=>	$arParams["CACHE_TIME"],
+                        "CACHE_FILTER"	=>	$arParams["CACHE_FILTER"],
+                        "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+                        "DISPLAY_TOP_PAGER"	=>	$arParams["DISPLAY_TOP_PAGER"],
+                        "DISPLAY_BOTTOM_PAGER"	=>	$arParams["DISPLAY_BOTTOM_PAGER"],
+                        "PAGER_TITLE"	=>	$arParams["PAGER_TITLE"],
+                        "PAGER_TEMPLATE"	=>	$arParams["PAGER_TEMPLATE"],
+                        "PAGER_SHOW_ALWAYS"	=>	$arParams["PAGER_SHOW_ALWAYS"],
+                        "PAGER_DESC_NUMBERING"	=>	$arParams["PAGER_DESC_NUMBERING"],
+                        "PAGER_DESC_NUMBERING_CACHE_TIME"	=>	$arParams["PAGER_DESC_NUMBERING_CACHE_TIME"],
+                        "PAGER_SHOW_ALL" => $arParams["PAGER_SHOW_ALL"],
+                        "DISPLAY_DATE"	=>	$arParams["DISPLAY_DATE"],
+                        "DISPLAY_NAME"	=>	$arParams["DISPLAY_NAME"],
+                        "DISPLAY_PICTURE"	=>	$arParams["DISPLAY_PICTURE"],
+                        "DISPLAY_PREVIEW_TEXT"	=>	$arParams["DISPLAY_PREVIEW_TEXT"],
+                        "PREVIEW_TRUNCATE_LEN"	=>	$arParams["PREVIEW_TRUNCATE_LEN"],
+                        "ACTIVE_DATE_FORMAT"	=>	$arParams["LIST_ACTIVE_DATE_FORMAT"],
+                        "USE_PERMISSIONS"	=>	$arParams["USE_PERMISSIONS"],
+                        "GROUP_PERMISSIONS"	=>	$arParams["GROUP_PERMISSIONS"],
+                        "FILTER_NAME"	=>	$arParams["FILTER_NAME"],
+                        "HIDE_LINK_WHEN_NO_DETAIL"	=>	$arParams["HIDE_LINK_WHEN_NO_DETAIL"],
+                        "CHECK_DATES"	=>	$arParams["CHECK_DATES"],
+                        "PARENT_SECTION" => $arResult["VARIABLES"]["SECTION_ID"],
+                        "PARENT_SECTION_CODE" => $arResult["VARIABLES"]["SECTION_CODE"],
+                    ),
+                    $component
+                );?>
+            <?endif;?>
+            <?// section elements?>
+            <?include_once("include_filter.php");?>
+            <?
+            $frame = new \Bitrix\Main\Page\FrameHelper('catalog-elements-block');
+            $frame->begin();
+            $frame->setAnimation(true);
+            ?>
+            <?//include_once("include_sort.php");?>
+            <?//$display_template = $display ? 'catalog-'.$display: 'catalog-table';?>
+            <?$display_template = 'catalog-table';?>
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:news.list",
+                $display_template,
+                array(
+                    "S_ASK_QUESTION" => $arParams["S_ASK_QUESTION"],
+                    "S_ORDER_PRODUCT" => $arParams["S_ORDER_PRODUCT"],
+                    "S_DETAIL_PRODUCT" => $arParams["S_DETAIL_PRODUCT"],
+                    "T_GALLERY" => $arParams["T_GALLERY"],
+                    "T_DOCS" => $arParams["T_DOCS"],
+                    "T_PROJECTS" => $arParams["T_PROJECTS"],
+                    "T_CHARACTERISTICS" => $arParams["T_CHARACTERISTICS"],
+                    "IBLOCK_TYPE"	=>	$arParams["IBLOCK_TYPE"],
+                    "IBLOCK_ID"	=>	$arParams["IBLOCK_ID"],
+                    "NEWS_COUNT"	=>	$arParams["NEWS_COUNT"],
+                    "SORT_BY1"	=>	$arAvailableSort[$sort]["SORT"],
+                    "SORT_ORDER1"	=>	strtoupper($order),
+                    "SORT_BY2"	=>	$arParams["SORT_BY2"],
+                    "SORT_ORDER2"	=>	$arParams["SORT_ORDER2"],
+                    "FIELD_CODE"	=>	$arParams["LIST_FIELD_CODE"],
+                    "PROPERTY_CODE"	=>	$arParams["LIST_PROPERTY_CODE"],
+                    "SHOW_SLIDE_PROP"	=>	$arParams["SHOW_SLIDE_PROP"],
+                    "DISPLAY_PANEL"	=>	$arParams["DISPLAY_PANEL"],
+                    "SET_TITLE"	=>	$arParams["SET_TITLE"],
+                    "SET_STATUS_404" => $arParams["SET_STATUS_404"],
+                    "INCLUDE_IBLOCK_INTO_CHAIN"	=>	$arParams["INCLUDE_IBLOCK_INTO_CHAIN"],
+                    "ADD_SECTIONS_CHAIN"	=>	$arParams["ADD_SECTIONS_CHAIN"],
+                    "CACHE_TYPE"	=>	$arParams["CACHE_TYPE"],
+                    "CACHE_TIME"	=>	$arParams["CACHE_TIME"],
+                    "CACHE_FILTER"	=>	$arParams["CACHE_FILTER"],
+                    "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+                    "DISPLAY_TOP_PAGER"	=>	$arParams["DISPLAY_TOP_PAGER"],
+                    "DISPLAY_BOTTOM_PAGER"	=>	$arParams["DISPLAY_BOTTOM_PAGER"],
+                    "PAGER_TITLE"	=>	$arParams["PAGER_TITLE"],
+                    "PAGER_TEMPLATE"	=>	$arParams["PAGER_TEMPLATE"],
+                    "PAGER_SHOW_ALWAYS"	=>	$arParams["PAGER_SHOW_ALWAYS"],
+                    "PAGER_DESC_NUMBERING"	=>	$arParams["PAGER_DESC_NUMBERING"],
+                    "PAGER_DESC_NUMBERING_CACHE_TIME"	=>	$arParams["PAGER_DESC_NUMBERING_CACHE_TIME"],
+                    "PAGER_SHOW_ALL" => $arParams["PAGER_SHOW_ALL"],
+                    "DISPLAY_DATE"	=>	$arParams["DISPLAY_DATE"],
+                    "DISPLAY_NAME"	=>	$arParams["DISPLAY_NAME"],
+                    "DISPLAY_PICTURE"	=>	$arParams["DISPLAY_PICTURE"],
+                    "DISPLAY_PREVIEW_TEXT"	=>	$arParams["DISPLAY_PREVIEW_TEXT"],
+                    "PREVIEW_TRUNCATE_LEN"	=>	$arParams["PREVIEW_TRUNCATE_LEN"],
+                    "ACTIVE_DATE_FORMAT"	=>	$arParams["LIST_ACTIVE_DATE_FORMAT"],
+                    "USE_PERMISSIONS"	=>	$arParams["USE_PERMISSIONS"],
+                    "GROUP_PERMISSIONS"	=>	$arParams["GROUP_PERMISSIONS"],
+                    "FILTER_NAME"	=>	$arParams["FILTER_NAME"],
+                    "HIDE_LINK_WHEN_NO_DETAIL"	=>	$arParams["HIDE_LINK_WHEN_NO_DETAIL"],
+                    "CHECK_DATES"	=>	$arParams["CHECK_DATES"],
+                    "PARENT_SECTION"	=>	$arResult["VARIABLES"]["SECTION_ID"],
+                    "PARENT_SECTION_CODE"	=>	$arResult["VARIABLES"]["SECTION_CODE"],
+                    "DETAIL_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["detail"],
+                    "SECTION_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
+                    "IBLOCK_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["news"],
+                    "INCLUDE_SUBSECTIONS" => "N",
+                    "SHOW_DETAIL_LINK" => $arParams["SHOW_DETAIL_LINK"],
+                    "COUNT_IN_LINE" => "3",
+                    "IMAGE_POSITION" => "left",
+                    "SHOW_PRICE_BLOCK" => $showPriceBlock
+                ),
+                $component
+            );?>
+            <?if($arSubSections && ((int)$arResult['VARIABLES']['SECTION_ID'] === 13)):?>
+                <?// sections list?>
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:news.list",
+                    $sectionTemplate,
+                    Array(
+                        "IBLOCK_TYPE"	=>	$arParams["IBLOCK_TYPE"],
+                        "IBLOCK_ID"	=>	$arParams["IBLOCK_ID"],
+                        "NEWS_COUNT"	=>	$arParams["NEWS_COUNT"],
+                        "SORT_BY1"	=>	$arParams["SORT_BY1"],
+                        "SORT_ORDER1"	=>	$arParams["SORT_ORDER1"],
+                        "SORT_BY2"	=>	$arParams["SORT_BY2"],
+                        "SORT_ORDER2"	=>	$arParams["SORT_ORDER2"],
+                        "FIELD_CODE"	=>	$arParams["LIST_FIELD_CODE"],
+                        "PROPERTY_CODE"	=>	$arParams["LIST_PROPERTY_CODE"],
+                        "DETAIL_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["detail"],
+                        "SECTION_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
+                        "IBLOCK_URL"	=>	$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["news"],
+                        "DISPLAY_PANEL"	=>	$arParams["DISPLAY_PANEL"],
+                        "SET_TITLE"	=>	"N",
+                        "SET_STATUS_404" => "N",
+                        "INCLUDE_IBLOCK_INTO_CHAIN"	=>	"N",
+                        "ADD_SECTIONS_CHAIN"	=>	"N",
+                        "CACHE_TYPE"	=>	$arParams["CACHE_TYPE"],
+                        "CACHE_TIME"	=>	$arParams["CACHE_TIME"],
+                        "CACHE_FILTER"	=>	$arParams["CACHE_FILTER"],
+                        "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+                        "DISPLAY_TOP_PAGER"	=>	$arParams["DISPLAY_TOP_PAGER"],
+                        "DISPLAY_BOTTOM_PAGER"	=>	$arParams["DISPLAY_BOTTOM_PAGER"],
+                        "PAGER_TITLE"	=>	$arParams["PAGER_TITLE"],
+                        "PAGER_TEMPLATE"	=>	$arParams["PAGER_TEMPLATE"],
+                        "PAGER_SHOW_ALWAYS"	=>	$arParams["PAGER_SHOW_ALWAYS"],
+                        "PAGER_DESC_NUMBERING"	=>	$arParams["PAGER_DESC_NUMBERING"],
+                        "PAGER_DESC_NUMBERING_CACHE_TIME"	=>	$arParams["PAGER_DESC_NUMBERING_CACHE_TIME"],
+                        "PAGER_SHOW_ALL" => $arParams["PAGER_SHOW_ALL"],
+                        "DISPLAY_DATE"	=>	$arParams["DISPLAY_DATE"],
+                        "DISPLAY_NAME"	=>	$arParams["DISPLAY_NAME"],
+                        "DISPLAY_PICTURE"	=>	$arParams["DISPLAY_PICTURE"],
+                        "DISPLAY_PREVIEW_TEXT"	=>	$arParams["DISPLAY_PREVIEW_TEXT"],
+                        "PREVIEW_TRUNCATE_LEN"	=>	$arParams["PREVIEW_TRUNCATE_LEN"],
+                        "ACTIVE_DATE_FORMAT"	=>	$arParams["LIST_ACTIVE_DATE_FORMAT"],
+                        "USE_PERMISSIONS"	=>	$arParams["USE_PERMISSIONS"],
+                        "GROUP_PERMISSIONS"	=>	$arParams["GROUP_PERMISSIONS"],
+                        "FILTER_NAME"	=>	$arParams["FILTER_NAME"],
+                        "HIDE_LINK_WHEN_NO_DETAIL"	=>	$arParams["HIDE_LINK_WHEN_NO_DETAIL"],
+                        "CHECK_DATES"	=>	$arParams["CHECK_DATES"],
+                        "PARENT_SECTION" => $arResult["VARIABLES"]["SECTION_ID"],
+                        "PARENT_SECTION_CODE" => $arResult["VARIABLES"]["SECTION_CODE"],
+                    ),
+                    $component
+                );?>
+            <?endif;?>
+            <?if(!empty($arSection['DESCRIPTION']) && strpos($_SERVER["REQUEST_URI"], "PAGEN") === false):?>
+                <div style="clear: both; display: block"></div>
+                <div class="cat-desc"><?=$arSection["DESCRIPTION"]?></div>
+            <?endif;?>
+        </div>
     </div>
 	<?$frame->end();?>
 <?endif;?>
