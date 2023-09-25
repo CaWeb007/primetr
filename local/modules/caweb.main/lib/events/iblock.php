b3cb7766e3e9cf0f9be89550ab0ee8ca4b3be52a
CaWeb007 <spcell007@gmail.com>
2023-09-25 09:20:43 +0800

    нашел микробаг

--- local/modules/caweb.main/lib/events/iblock.php
+++ local/modules/caweb.main/lib/events/iblock.php
@@ -14,7 +14,7 @@ use Caweb\Main\Log\Write;
 
 class Iblock{
     const FORMS_ID = array(16, 17, 14, 12, 33, 37);
-    const BANNER_IBLOCK_ID = 35;
+    const BANNER_IBLOCK_ID = 34;
     const MAIN_BANNERS_IBLOCK_ID = 35;
     const CONTENT_IBLOCK_TYPE = 'aspro_stroy_content';
     const ADV_IBLOCK_TYPE= 'aspro_stroy_content';
