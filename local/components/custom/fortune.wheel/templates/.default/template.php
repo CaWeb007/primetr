f08ec5a9c1da539cd0df1f468e42ac4e74dcf412
p.reutov <reutov@caweb.ru>
2025-02-12 15:39:46 +0800

    refactor step 2

--- local/components/custom/fortune.wheel/templates/.default/template.php
+++ local/components/custom/fortune.wheel/templates/.default/template.php
@@ -1,6 +1,6 @@
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-/** @var array $arResult */
+/** @var array $arResult[PRIZES=>array, COOKIE_PRIZE=>string] */
 $countPrizes = count($arResult['PRIZES']);
 ?>
 
